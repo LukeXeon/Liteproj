0fba620ffc27c4a8d9d8a492dd07ace3726f464b
Luke <694014820@qq.com>
2019-01-31 23:55:15 +0800

    update

--- framework/src/main/java/org/kexie/android/arch/automatic/dependency/DependenciesManager.java
+++ framework/src/main/java/org/kexie/android/arch/automatic/dependency/DependenciesManager.java
@@ -4,8 +4,6 @@ import android.app.Activity;
 import android.app.Application;
 import android.content.Context;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -158,5 +156,4 @@ public final class DependenciesManager
             type = type.getSuperclass();
         }
     }
-
 }
